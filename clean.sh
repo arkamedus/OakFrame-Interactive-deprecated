08dea0b6a16d4174fb9f198930eb2271559bf488
Gordon Goodrum <stopgordy@gmail.com>
2020-03-20 17:01:43 -0500

    NPM Package Init

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+find ./lib -type f -name '*.js' -delete
+find ./lib -type f -name '*.js.map' -delete
+find ./lib -type f -name '*.js 2.map' -delete
+find ./lib -type f -name '*.js 2' -delete
